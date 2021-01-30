bcaa19c2a55f5e719e7c7883c86d9a4c304d664c
MirtoSpatha <59802327+MirtoSpatha@users.noreply.github.com>
2021-01-30 02:10:11 +0200

    Add files via upload

---
+++ FormMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExamGenerator.CustomControls;
+
+namespace ExamGenerator
+{
+    public partial class FormMain : Form
+    {
+        public Subject subject;
+        string username;
+
+        public FormMain(Subject subject, string username)
+        {
+            InitializeComponent();
+            previousSize = Size; // Used to make user controls responsive
+            this.subject = subject;
+            this.username = username;
+            questionsSection.subject = subject;
+            addQuestionsSection1.subject = subject;
+            // If the screen is smaller than the form size start fullscreen
+            //if (Screen.GetWorkingArea(this).Width > this.Width || 
+            //    Screen.GetWorkingArea(this).Height > this.Height )
+            //{
+            //    WindowState = FormWindowState.Maximized;
+            //}
+        }
+
+        /* Gives the focus to the search textbox when a control in 
+         * the search area is clicked */
+        private void searchArea_Click(object sender, EventArgs e)
+        {
+            textBoxSearch.Focus();
+        }
+
+        /* Menu functionality */
+        private void radioMenuQuestions_Click(object sender, EventArgs e)
+        {
+            // Show the color panel associated with the selected menu radio button
+            TogglablePanel.ShowPanel(panelMenuButtons.Controls, panelQuestionsActive);
+            questionsSection.BringToFront();
+        }
+
+        private void radioMenuAddQuestions_Click(object sender, EventArgs e)
+        {
+            TogglablePanel.ShowPanel(panelMenuButtons.Controls, panelAddQuestionsActive);
+            addQuestionsSection1.BringToFront();
+        }
+
+        private void radioMenuManual_Click(object sender, EventArgs e)
+        {
+            TogglablePanel.ShowPanel(panelMenuButtons.Controls, panelManualActive);
+        }
+
+        private void radioMenuAuto_Click(object sender, EventArgs e)
+        {
+            TogglablePanel.ShowPanel(panelMenuButtons.Controls, panelAutoActive);
+        }
+
+        // Responsiveness
+        Size previousSize;
+        private void FormMain_ResizeEnd(object sender, EventArgs e)
+        {
+            if (Size != previousSize)
+            {
+                questionsSection.OnResizeEnd();
+                previousSize = Size;
+            }
+                
+        }
+
+        FormWindowState previousState = FormWindowState.Normal;
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            if (WindowState != previousState && WindowState != FormWindowState.Minimized)
+            {
+                questionsSection.OnResizeEnd();
+                previousState = WindowState;
+            }
+        }
+
+        private void addQuestionsSection1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Serialize.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamGenerator
+{
+    public static class Serialize
+    {
+        static string savePath;
+
+        static Serialize()
+        {
+            var commonAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            savePath = Path.Combine(commonAppDataPath, "Exam Generator");
+            Directory.CreateDirectory(savePath);
+        }
+
+        private static string GetFullPath(string fileName) 
+        {
+            return Path.Combine(savePath, fileName);
+        }
+
+        public static void SaveSubjects(Dictionary<string, Subject> subjects) 
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(GetFullPath("subjects.eg"), FileMode.OpenOrCreate, FileAccess.Write);
+
+            formatter.Serialize(stream, subjects);
+
+            stream.Close();
+        }
+
+        public static Dictionary<string, Subject> LoadSubjects()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(GetFullPath("subjects.eg"), FileMode.OpenOrCreate, FileAccess.Read);
+
+            Dictionary<string, Subject> subjects;
+            if (stream.Length > 0)
+                subjects = (Dictionary<string, Subject>)formatter.Deserialize(stream);
+            else
+                subjects = null;
+
+            stream.Close();
+            return subjects;
+        }
+    }
+}
