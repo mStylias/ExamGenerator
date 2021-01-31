5e5051775b3eaeb3852407095b56af0b8b7cf191
MirtoSpatha <mirtospatha173@gmail.com>
2021-01-31 19:33:54 +0200

    add question section completed

--- ExamGenerator-Mirto/ExamGenerator/MainFiles/AddQuestionsSection.cs
+++ ExamGenerator-Mirto/ExamGenerator/MainFiles/AddQuestionsSection.cs
@@ -16,16 +16,12 @@ namespace ExamGenerator.MainFiles
         public AddQuestionsSection()
         {
             InitializeComponent();
-            /*listBox1.Items.Add("No1");
-            listBox1.Items.Add("No2");
-            listBox1.Items.Add("No3");*/
         }
 
         private void AddQuestionsSection_Load(object sender, EventArgs e)
         {
             //listBox1.Update();
             ////listBox1.DataSource = subject.SubjectTags;
-            
         }
 
         public void OnLoad(Subject subject)
@@ -53,7 +49,10 @@ namespace ExamGenerator.MainFiles
                     tags.Add(richTextBox4.Text);
                 }
                 List<string> answers = new List<string>();
-                answers.AddRange(richTextBoxAnswers.Text.Split(Environment.NewLine.ToCharArray()));
+                foreach(RichTextBox textbox in panel3.Controls.OfType<RichTextBox>())
+                {
+                    answers.Add(textbox.Text);
+                }
                 Question q = new Question(subject, tags, richTextBoxQuestion.Text, answers, richTextBoxCorrectAnswer.Text, comboBox1.SelectedItem.ToString());
                 if (q != null) MessageBox.Show("Question added successfully!");
             }
@@ -63,5 +62,83 @@ namespace ExamGenerator.MainFiles
             }
         }
 
+        static int textboxCounter = 2; // Used to name the new textboxes correctly
+        private void AddAnswerButton_Click(object sender, EventArgs e)
+        {
+            // Locations
+            int addQuestionY = AddAnswerButton.Location.Y + 70;
+            int addQuestionX = AddAnswerButton.Location.X;
+            AddAnswerButton.Location = new Point(addQuestionX, addQuestionY);
+
+            int answerTextboxY = addQuestionY - 70;
+
+            // Advance the counter
+            textboxCounter++;
+
+            // Make the new textbox
+            RichTextBox t = new RichTextBox
+            {
+                Anchor = AnchorStyles.Top,
+                Dock = DockStyle.Top,
+                Font = new Font("Century Gothic", 11),
+                Location = new Point(addQuestionX, answerTextboxY),
+                Margin = new Padding(3, 0, 3, 0),
+                Multiline = true,
+
+                // Make the new textbox
+                Name = "AnswerTextBox" + textboxCounter,
+                Size = new Size(647, 40),
+                TabIndex = 0,
+                Text = textboxCounter + "."
+            };
+
+            // Add the new text box to panelAddQuestion
+            panel3.Controls.Add(t);
+            panel3.Controls.SetChildIndex(t, 2);
+            foreach (Control c in panel1.Controls)
+            {
+                if(c.TabIndex > panel3.TabIndex)
+                {
+                    //MessageBox.Show(c.Name + " " + c.TabIndex);
+                    c.Location = new Point(c.Location.X, c.Location.Y + 40);
+                }
+            }
+            
+            // Make remove button visible
+            RemoveAnswerButton.Visible = true;
+
+            // Move remove button
+            RemoveAnswerButton.Location = new Point(RemoveAnswerButton.Location.X, RemoveAnswerButton.Location.Y + 40);
+        }
+
+        private void RemoveAnswerButton_Click(object sender, EventArgs e)
+        {
+            foreach (Control textBox in panel3.Controls.OfType<RichTextBox>())
+            {
+
+                if (textboxCounter < 3)
+                {
+                    break;
+                }
+                else if (textBox.Name.Equals("AnswerTextBox" + textboxCounter))
+                {
+                    // Remove the textBox
+                    panel3.Controls.Remove(textBox);
+                    // Move the AddQuestion button
+                    AddAnswerButton.Location = new Point(AddAnswerButton.Location.X, AddAnswerButton.Location.Y - 40);
+                    // Move the RemoveAnswer button
+                    RemoveAnswerButton.Location = new Point(RemoveAnswerButton.Location.X, RemoveAnswerButton.Location.Y - 40);
+
+                    textboxCounter--;
+
+                    // Make the remove button invisible when there are only 2 textboxes so it doesnt remove more
+                    if (textboxCounter < 3)
+                    {
+                        RemoveAnswerButton.Visible = false;
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
